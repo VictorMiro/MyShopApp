82b903b3ae507c412157d168748785149ff03029
VictorMiro <viktor.myroshnychenko@nure.ua>
2018-11-16 01:03:01 +0200

    populated the database with data

--- SHOP/admin.py
+++ SHOP/admin.py
@@ -3,7 +3,7 @@ from SHOP.models import TradeMark, Products, ModelType, GadgetType
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'count')
+    list_display = ('id', 'name', 'price', 'count', 'trade_mark', 'model_type', 'gadget_type')
     search_fields = ('name', 'trade_mark__name', 'model_type__name')
 
 
